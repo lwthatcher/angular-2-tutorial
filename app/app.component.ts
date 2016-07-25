5c01a39bd98e3960ae1ec6ff6882d0a43127c70e
Lawrence Thatcher <lwthatcher@msn.com>
2016-07-25 11:58:52 -0600

    finished tutoral 6

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,12 +9,13 @@ import { HeroService } from './hero.service';
 		<h1>{{title}}</h1>
 		<nav>
 			<a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-			<a [routerLink]="['/heroes']">Heroes</a>
+			<a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
 		</nav>
 		<router-outlet></router-outlet>
 		`,
 	directives: [ROUTER_DIRECTIVES],
-	providers: [HeroService]
+	providers: [HeroService],
+	styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent {
